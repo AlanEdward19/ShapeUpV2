2bb98448fe8b2c931ed52633cc61c2deee103c8d
AlanEdward19 <alan-edward@outlook.com.br>
2026-09-17 08:37:11 -0300

    feat(training): migrate ExerciseEquivalents table

---
+++ src/Features/Training/Infrastructure/Data/Migrations/20260917113659_AddExerciseEquivalents.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShapeUp.Features.Training.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExerciseEquivalents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExerciseEquivalents",
+                columns: table => new
+                {
+                    ExerciseId = table.Column<int>(type: "int", nullable: false),
+                    EquivalentExerciseId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExerciseEquivalents", x => new { x.ExerciseId, x.EquivalentExerciseId });
+                    table.ForeignKey(
+                        name: "FK_ExerciseEquivalents_Exercises_EquivalentExerciseId",
+                        column: x => x.EquivalentExerciseId,
+                        principalTable: "Exercises",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ExerciseEquivalents_Exercises_ExerciseId",
+                        column: x => x.ExerciseId,
+                        principalTable: "Exercises",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExerciseEquivalents_EquivalentExerciseId",
+                table: "ExerciseEquivalents",
+                column: "EquivalentExerciseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExerciseEquivalents");
+        }
+    }
+}
